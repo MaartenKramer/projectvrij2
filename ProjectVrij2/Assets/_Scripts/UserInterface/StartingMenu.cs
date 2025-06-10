24b2a804dbe16a23893f6f739b5164c393e3be07
SayonaraKuro <sayonara.kuroi@gmail.com>
2025-06-10 06:54:16 +0200

    Added logo + controls panel
    - Logo added to start menu
    - Added button to view controls (still need descriptions added)

--- ProjectVrij2/Assets/_Scripts/UserInterface/StartingMenu.cs
+++ ProjectVrij2/Assets/_Scripts/UserInterface/StartingMenu.cs
@@ -3,15 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class StartingMenu : MonoBehaviour
 {
+    public GameObject controlsCanvas;
+    public GameObject mainMenuCanvas;
+
     //Load next scene, start the game
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Debug.Log("Loading next scene...");
     }
+
+    public void controlPanel()
+    {
+        mainMenuCanvas.SetActive(false);
+        controlsCanvas.SetActive(true);
+    }
     public void QuitGame()
     {
         Debug.Log("Quitting game...");
         Application.Quit();
+    }
+    public void mainMenuPanel()
+    {
+        controlsCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(true);
     }
 }
